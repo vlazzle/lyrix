778b87800a1e32852ca505539c28b29de1b63e87
vlad <iamvlad@gmail.com>
2011-09-17 23:41:15 -0700

    on initial load, show all comments (with limit) in right side; lots of refactoring

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,91 +1,137 @@
-function randomMusicalSymbol() {
-  var choices = '9abc';
-  return [
-    '&#x266',
-    choices[Math.floor(Math.random() * choices.length)],
-    ';'
-  ].join('');
-}
+var Comments = (function() {
+  function randomMusicalSymbol() {
+    var choices = '9abc';
+    return [
+      '&#x266',
+      choices[Math.floor(Math.random() * choices.length)],
+      ';'
+    ].join('');
+  }
 
-function updateBox($box, zeroWidth) {
-  $box.css({
-    top: 0,
-    left: $('#main').width(),
-    width: zeroWidth ? 0 : halfWidth(),
-    height: $(window).height()
-  });
-}
+  function updateParentBox($box, zeroWidth) {
+    $box.parent().css({
+      top: 0,
+      left: $('#main').width(),
+      width: zeroWidth ? 0 : halfWidth(),
+      height: $(window).height()
+    });
+  }
 
-function halfWidth() {
-  return $('body').width() - $('#main').width()
-}
+  function halfWidth() {
+    return $('body').width() - $('#main').width();
+  }
 
-function trim(str) {
-  return str.trim().replace(/\W*$/, '');
-}
+  function trim(str) {
+    return str.trim().replace(/\W*$/, '');
+  }
 
-$(function() {
-  conf = {
-    songId: $('#lyrics').data('song_id')
-  };
-  
-  $('.line').hover(function(e) {
-    $('.add_comments', this).toggleClass('show');
-  });
-  
-  $('.view_comments a').click(function(e) {
-    var $line = $(e.target).parents('.line'),
-        lineno = $line.attr('id').slice(1) - 1.
-        $lyrics = $('#lyrics'),
-        $lyric = $('.lyric', $line);
+  function showSomeComments(xhrGetCallback) {
+    _showComments(xhrGetCallback);
+  }
 
-    $('#comment_list').remove();
-    $('.lyric', $lyrics).removeClass('red');
-    
-    var $commentList = $([
-      '<section id="comment_list"><div><h1><span class="gray">',
-      randomMusicalSymbol(),
+  function showCommentsForSelector($songLineSelector, xhrGetCallback) {
+    _showComments($songLineSelector, xhrGetCallback);
+  }
+
+  function _showComments(/* [$target], xhrGetCallback */) {
+    var params = {};
+
+    if (arguments.length === 2) {
+      var xhrGetCallback = arguments[1];
+          $target = arguments[0],
+          $line = $target.parents('.line'),
+          lineno = $line.attr('id').slice(1) - 1,
+          $lyrics = $('#lyrics'),
+          $lyric = $('.lyric', $line),
+          header = trim($lyric.text());
+
+      $('.lyric', $lyrics).removeClass('red');
+      $lyric.addClass('red');
+
+      params.line = lineno;
+    }
+    else {
+      var xhrGetCallback = arguments[0],
+          header = null;
+    }
+
+    var $commentList = $('#comment_list div');
+
+    // TODO use template
+    $commentList.html([
+      '<h1><span class="gray">',
+      Comments.randomMusicalSymbol(),
       '</span> ',
-      trim($lyric.text()),
-      '&hellip; <span class="gray">',
-      randomMusicalSymbol(),
-      '</span></h1>',
-      '</div></section>'
-    ].join('')).prependTo('body');
-    
-    var top = Math.min($lyric.position().top, Math.max($lyrics.position().top, $(window).scrollTop()));
-    
-    $commentList.css({
+      header || 'All Comments',
+      '<span>&hellip; </span><span class="gray">',
+      Comments.randomMusicalSymbol(),
+      '</span></h1>'
+    ].join(''));
+
+    !header && $('span', $commentList).hide();
+
+    $commentList.parent().css({
       display: 'block',
       position: 'fixed'
     });
-    updateBox($commentList, true);
-    
-    $lyric.addClass('red');
-    $commentList.animate({
-      width: halfWidth(),
+    Comments.updateParentBox($commentList, true);
+
+    $commentList.parent().animate({
+      width: Comments.halfWidth()
     }, 200);
 
-    $.get('/songs/' + conf.songId + '/comments', {line:lineno}, function(data) {
-      $('div', $commentList).append(data);
-    });
-    return false;
+    $.get('/songs/' + Song.id + '/comments', params, xhrGetCallback);
+  }
+
+  return {
+    randomMusicalSymbol: randomMusicalSymbol,
+    updateParentBox: updateParentBox,
+    halfWidth: halfWidth,
+    trim: trim,
+    showSomeComments: showSomeComments,
+    showCommentsForSelector: showCommentsForSelector
+  };
+}());
+
+$(function() {
+  Song = {
+    id: $('#lyrics').data('song_id')
+  };
+
+  $('.line').hover(function(e) {
+    $('.add_comments', this).toggleClass('show');
   });
-  
+
+/*
   $('#main').scroll(function() {
     // check if we're at the bottom of the scrollable box
     var yPadding = parseInt($('#main2').css('padding-top')) + parseInt($('#main2').css('padding-bottom'));
     if ($('#main').scrollTop() >= $('#main2').height() - $(window).height() + yPadding - 1) {
-      $.get('/songs/' + conf.songId + '/comments', function(data) {
+      $.get('/songs/' + Song.id + '/comments', function(data) {
         $('#discussion').append(data);
-        
+
         // this scroll event handler should only be triggered once
         $('#main').unbind('scroll');
       });
     }
   });
-  
+*/
+
+  var $commentList = $('#comment_list div');
+
+  $('.view_comments a').click(function(e) {
+    Comments.showCommentsForSelector($(e.target), function(data) {
+      $commentList.append(data);
+    });
+    return false;
+  });
+
   $(window).resize(function() {
-    updateBox($('#comment_list'), false);
+    Comments.updateParentBox($commentList, false);
+  });
+
+  Comments.showSomeComments(function(data) {
+    $commentList.append(data);
+    $('#discussion').append(data);
   });
 });
